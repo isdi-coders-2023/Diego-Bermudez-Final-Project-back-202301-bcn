3ef9192fc9f57835f175dae89897153bd4044052
DI3BOSS <diegofbermudez@gmail.com>
2023-03-06 20:20:08 +0100

    Implement a basic structure for middlewares chain index

---
+++ src/server/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import options from "../cors";
+
+const root = "/";
+const userEndpoint = "/users";
+
+export const app = express();
+
+app.disable("x-powered-by");
+
+app.use(cors(options));
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use();
