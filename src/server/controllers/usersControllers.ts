bc2b0613a32956e71df38b80460926b3c6e0de74
DI3BOSS <diegofbermudez@gmail.com>
2023-03-06 20:32:38 +0100

    Implement getUsers controller

---
+++ src/server/controllers/usersControllers.ts
@@ -0,0 +1,17 @@
+import { type NextFunction, type Request, type Response } from "express";
+import CustomError from "../../CustomError/CustomError";
+import User from "../../database/models/User";
+
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find().exec();
+
+    res.status(200).json({ users });
+  } catch (error) {
+    next(new CustomError(error as string, 500, "Couldn't retrieve users."));
+  }
+};
