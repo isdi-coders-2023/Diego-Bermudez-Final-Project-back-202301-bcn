9b4099dd9d2ec27d32cca0377025b52c2b7264ff
DI3BOSS <diegofbermudez@gmail.com>
2023-03-07 12:11:43 +0100

    Add usersRouter to middlewares chain index

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,10 @@ import morgan from "morgan";
 import cors from "cors";
 import options from "../cors.js";
 import gamesRouter from "./routers/gamesRouters.js";
+import usersRouter from "./routers/usersRouters.js";
 
-const gameEndpoint = "/games";
+const gamesEndpoint = "/games";
+const usersEndpoint = "/users";
 
 export const app = express();
 
@@ -14,4 +16,5 @@ app.use(cors(options));
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use(gameEndpoint, gamesRouter);
+app.use(gamesEndpoint, gamesRouter);
+app.use(usersEndpoint, usersRouter);
