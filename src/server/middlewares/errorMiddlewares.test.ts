c17415c973d27c96950879045f8554c825433def
DI3BOSS <diegofbermudez@gmail.com>
2023-03-09 11:39:19 +0100

    Test the generalError  middleware

--- src/server/middlewares/errorMiddlewares.test.ts
+++ src/server/middlewares/errorMiddlewares.test.ts
@@ -1,5 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
-import { notFoundError } from "./errorMiddlewares.js";
+import CustomError from "../../CustomError/CustomError.js";
+import errors from "../constants/errors.js";
+import { generalError, notFoundError } from "./errorMiddlewares.js";
 
 const response: Partial<Response> = {
   status: jest.fn().mockReturnThis(),
@@ -18,6 +20,24 @@ describe("Given the notFoundError middleware", () => {
       notFoundError(request as Request, response as Response, next);
 
       expect(next).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a generalError middleware", () => {
+  describe("When it receives an error with status 500", () => {
+    test("Then it shoudl call its status method with a 500", () => {
+      const { statusCode } = errors.serverError;
+
+      const error = new CustomError(
+        errors.serverError.message,
+        errors.serverError.statusCode,
+        errors.serverError.publicMessage
+      );
+
+      generalError(error, request as Request, response as Response, next);
+
+      expect(response.status).toHaveBeenCalledWith(statusCode);
     });
   });
 });
